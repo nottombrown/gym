2e2a76f281ede2967311df1d480a4897917787f5
Tom Brown <nottombrown@gmail.com>
2016-06-11 22:24:33 -0700

    Remove redundant tests

--- gym/envs/starcraft/tests/test_starcraft_image_file.py
+++ gym/envs/starcraft/tests/test_starcraft_image_file.py
@@ -1,5 +1,4 @@
 import unittest
-from io import BytesIO
 from numpy import testing as np_test
 import numpy as np
 from PIL import Image
@@ -26,37 +25,13 @@ class StarCraftImageFileTest(unittest.TestCase):
     def test_palette_serialization(self):
         new_img = StarCraftImageFile.from_np_array(self.img.to_obs())
 
-        # Regression - Palettes are the same as a scif
+        # Regression - Make sure that palettes are the same
         self.assertEqual(list(self.img.getpalette()), list(new_img.getpalette()))
-
-        # Regression - Bytes are the same when written as a png
-        old_bytes = BytesIO()
-        new_bytes = BytesIO()
-        self.img.save(old_bytes, format='png')
-        new_img.save(new_bytes, format='png')
-
-        self.assertEqual(list(old_bytes), list(new_bytes))
-
-        # Regression - Pixels and palettes are the same as png
-        old_image = Image.open(old_bytes)
-        new_image = Image.open(new_bytes)
-
-        self.assertEqual(list(old_image.getdata()), list(new_image.getdata()))
-        self.assertEqual(list(old_image.getpalette()), list(new_image.getpalette()))
 
     def test_to_np_rgb(self):
         np_rgb = self.img.to_np_rgb()
         self.assertEqual(np_rgb.shape, (480, 640, 3))
 
         # Test that pixels are correct
-        first_pixel = np.array([36, 40, 44], dtype=np.uint8)
-        np_test.assert_equal(np_rgb[0, 0, :], first_pixel)
-
-    def test_to_np_rgb_after_vectorizing(self):
-        # Regression test
-        new_img = StarCraftImageFile.from_np_array(self.img.to_obs())
-        np_rgb = new_img.to_np_rgb()
-
-        # Test that pixels stay the same after vectorizing
         first_pixel = np.array([36, 40, 44], dtype=np.uint8)
         np_test.assert_equal(np_rgb[0, 0, :], first_pixel)
