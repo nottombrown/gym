bcbe0908c36ad0cabc6c61f92f1e806defd0ee32
Tom Brown <nottombrown@gmail.com>
2016-06-11 22:13:01 -0700

    Set correct observation_space, rename game_client

--- gym/envs/starcraft/starcraft_basic_env.py
+++ gym/envs/starcraft/starcraft_basic_env.py
@@ -1,11 +1,8 @@
-import numpy as np
 import gym
 from gym import spaces
-from StringIO import StringIO
-from PIL import Image
-from gym.envs.starcraft.starcraft_image_file import StarCraftImageFile
-from PIL.PngImagePlugin import PngImageFile
-
+from gym.envs.starcraft.starcraft_image_file import StarCraftImageFile, \
+    starcraft_screen_height, \
+    starcraft_screen_width
 from remote_starcraft_game_client import RemoteStarCraftGameClient
 
 # To play yourself, run:
@@ -19,16 +16,26 @@ class StarCraftBasicEnv(gym.Env):
 
     def __init__(self):
         self.viewer = None
-        self.screen_height = 480
-        self.screen_width = 640
         self._obs = None # Cache the last observation we've seen
 
-        self.game = RemoteStarCraftGameClient()
-        self.game.reset()  # Initializes a new episode on the server
+        self.game_client = RemoteStarCraftGameClient()
+        self.game_client.reset()  # Initializes a new episode on the server
 
-        high = np.array([np.inf]*8)
         self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(-high, high)
+
+        # We use HighLow subspaces because they allow various ranges
+        action_subspaces = [
+            [[0, starcraft_screen_height, 0]],  # Mouse Y coordinate
+            [[0, starcraft_screen_width, 0]],  # Mouse X coordinate
+            [[0, 2, 0]],  # Mouse button is one of [off, left, right]
+            [[0, 40, 0]],  # There are 36 alphanumerics, plus [tab, space, esc, enter]
+            [[0, 1, 0]]   # Each of the three modifiers [shift, control, alt] can be pressed
+        ]
+        joint_subspaces = sum(action_subspaces, [])
+
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(starcraft_screen_height,
+                                                   starcraft_screen_width))
         self._reset()
 
     def _reset(self):
@@ -36,18 +43,7 @@ class StarCraftBasicEnv(gym.Env):
 
     # TODO: Switch this to using numpy actions rather than action payloads
     def _step(self, action_payload):
-
-        # TODO: Re-enable assertions
-        # assert action in [0,1,2,3], "%r (%s) invalid " % (action,type(action))
-
-        # if action_payload != 0: # 0 is the null action
-
-        observation, reward, done, info = self.game.step(action_payload)
-
-        if self._obs != None:
-            # Check that nothing weird is happening
-            assert(np.array_equal(self._obs, observation))
-
+        observation, reward, done, info = self.game_client.step(action_payload)
         self._obs = observation
 
         return observation, reward, done, info
