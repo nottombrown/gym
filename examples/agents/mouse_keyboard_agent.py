c520839dadb3bed5ea009c3a4d5f353ba7239216
Tom Brown <nottombrown@gmail.com>
2016-06-13 11:20:01 -0700

    Clean up MouseKeyboardAgent

--- examples/agents/mouse_keyboard_agent.py
+++ examples/agents/mouse_keyboard_agent.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
-import sys, gym, logging, time
+import logging
+import sys
+import time
+
+import gym
 from gym.envs.starcraft.mouse_keyboard_action import MouseKeyboardAction
+
+logger = logging.getLogger()
 
 
 class MouseKeyboardAgent(object):
@@ -11,41 +17,39 @@ class MouseKeyboardAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
         self._next_action = MouseKeyboardAction.null()
+        env.render()  # Initialize window
+        env.viewer.window.on_key_press = self._key_press
+        env.viewer.window.on_key_release = self._key_release
+        env.viewer.window.on_mouse_press = self._mouse_press
+        env.viewer.window.on_mouse_release = self._mouse_release
 
-        env.render() # TODO: Remove this and see if things still work
-        env.viewer.window.on_key_press = self.key_press
-        env.viewer.window.on_key_release = self.key_release
-        env.viewer.window.on_mouse_press = self.mouse_press
-        env.viewer.window.on_mouse_release = self.mouse_release
-
-    def act(self, observation, reward, done):
-        """ Do the action and reset the next_action"""
+    def act(self):
+        """ Do the last action that was requested and then reset _next_action """
         mouse_keyboard_action = self._next_action
         self._next_action = MouseKeyboardAction.null()
         return mouse_keyboard_action.to_np()
 
-    def key_press(self, key, modifiers):
+    def _key_press(self, key, modifiers):
         self._next_action = MouseKeyboardAction.from_key_event(key, modifiers)
 
-    def key_release(self, key, modifiers):
+    def _key_release(self, key, modifiers):
         self._next_action = MouseKeyboardAction.from_key_event(key, modifiers)
 
-    def mouse_press(self, x, y, button, modifiers):
-        print("Mouse pressed: " + str((x, y)))
+    def _mouse_press(self, x, y, button, modifiers):
+        logger.info("Mouse pressed: " + str((x, y)))
         self._next_action = MouseKeyboardAction.from_mouse_event(x, y, button, modifiers)
 
-    def mouse_release(self, x, y, button, modifiers):
-        print("Mouse released: " + str((x, y)))
+    def _mouse_release(self, x, y, button, modifiers):
+        logger.info("Mouse released: " + str((x, y)))
         self._next_action = MouseKeyboardAction.from_mouse_event(x, y, button, modifiers)
 
 if __name__ == '__main__':
     # You can optionally set up the logger. Also fine to set the level
     # to logging.DEBUG or logging.WARN if you want to change the
     # amount of output.
-    logger = logging.getLogger()
     logger.setLevel(logging.INFO)
 
-    env = gym.make('StarCraftBasic-v0' if len(sys.argv)<2 else sys.argv[1])
+    env = gym.make('StarCraftBasic-v0' if len(sys.argv) < 2 else sys.argv[1])
     agent = MouseKeyboardAgent(env.action_space)
 
     episode_count = 10
@@ -58,11 +62,9 @@ if __name__ == '__main__':
 
         for j in range(max_steps):
             env.render()
-            action = agent.act(ob, reward, done)
+            action = agent.act()
             ob, reward, done, _ = env.step(action)
             if done:
                 break
 
-            time.sleep(1.0/30) # Render at 30 fps
-
-    # TODO: insure that the environment gets closed correctly
+            time.sleep(1.0/30)  # Render at 30 fps
